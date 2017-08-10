ae261cc9b165b38a583a6e0b113c27410d5246f2
mohousch <mohousch@gmail.com>
2017-08-10 08:47:37 +0200

    - supplement to last commit.

--- plugins/icecast/icecast.cpp
+++ plugins/icecast/icecast.cpp
@@ -10,48 +10,6 @@ std::string icecasturl = "http://dir.xiph.org/yp.xml";
 const long int GET_ICECAST_TIMEOUT = 90; 		// list is about 500kB!
 CAudioPlayerGui tmpAudioPlayerGui;
 
-class CIceCast
-{
-	public:
-		CIceCast();
-		~CIceCast();
-
-		void readDir_ic(void);
-};
-
-//
-CIceCast::CIceCast()
-{
-}
-
-CIceCast::~CIceCast()
-{
-}
-
-void CIceCast::readDir_ic(void)
-{
-	dprintf(DEBUG_NORMAL, "CIceCast::readDir_ic\n");
-	
-	std::string answer = "";
-	std::cout << "CIceCast::readDir_ic: IC URL: " << icecasturl << std::endl;
-	
-	// get it!
-	CHintBox *scanBox = new CHintBox(LOCALE_AUDIOPLAYER_ADD_IC, g_Locale->getText(LOCALE_AUDIOPLAYER_RECEIVING_LIST)); // UTF-8
-	scanBox->paint();
-
-	//
-	if(::getUrl(icecasturl, answer, " ", GET_ICECAST_TIMEOUT))
-	{
-		xmlDocPtr answer_parser = parseXml(answer.c_str());
-		scanBox->hide();
-		tmpAudioPlayerGui.scanXmlData(answer_parser, "server_name", "listen_url", "bitrate", true);
-	}
-	else 
-		scanBox->hide();
-
-	delete scanBox;
-}
-
 //
 extern "C" void plugin_exec(void);
 extern "C" void plugin_init(void);
@@ -67,13 +25,16 @@ void plugin_del(void)
 
 void plugin_exec(void)
 {
-	CIceCast* icecastHandler = new CIceCast();
+	std::string answer = "";
 
-	icecastHandler->readDir_ic();
-	
-	tmpAudioPlayerGui.setTitle("Ice Cast");
-	tmpAudioPlayerGui.exec(NULL, "");
-
-	delete icecastHandler;
-	icecastHandler = NULL;
+	if(::getUrl(icecasturl, answer, " ", GET_ICECAST_TIMEOUT))
+	{
+		xmlDocPtr answer_parser = parseXml(answer.c_str());
+		
+		tmpAudioPlayerGui.scanXmlData(answer_parser, "server_name", "listen_url", "bitrate", true);
+		tmpAudioPlayerGui.setTitle("Ice Cast");
+		tmpAudioPlayerGui.exec(NULL, "");
+	}
+	else
+		HintBox(LOCALE_MESSAGEBOX_INFO, "can't load icecast list");
 }
