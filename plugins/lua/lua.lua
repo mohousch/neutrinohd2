0ffe2c87cf6202e9f379c98ac1be890838662d82
mohousch <mohousch@gmail.com>
2019-02-03 20:25:47 +0100

    rewirte lua and python sample.

--- plugins/lua/lua.lua
+++ plugins/lua/lua.lua
@@ -3,135 +3,156 @@ print("lua: Hello World")
 neutrino = require 'neutrino'
 
 -- messagebox
-mBox = neutrino.CMessageBox("CMessageBox: lua","first test\ntesting CMessageBox\ndas ist alles ;-)")
-mBox:exec(-1)
+function messageBox()
+	mBox = neutrino.CMessageBox("CMessageBox: lua","first test\ntesting CMessageBox\ndas ist alles ;-)")
+	mBox:exec(-1)
+end
 
 -- helpbox
---[[
-hbox = neutrino.CHelpBox()
-hbox:addLine("neutrino: lua")
-hbox:addSeparator()
-hbox:addLine("first test")
-hbox:addLine("testing CHelpBox\ndas ist alles ;-)")
-hbox:show("CHelpBox: lua")
-]]
+function helpBox()
+	hbox = neutrino.CHelpBox()
+	hbox:addLine("neutrino: lua")
+	hbox:addSeparator()
+	hbox:addLine("first test")
+	hbox:addLine("testing CHelpBox\ndas ist alles ;-)")
+	hbox:show("CHelpBox: lua")
+end
 
 -- filebrowser
---[[
-fbox= neutrino.CFileBrowser('/')
-fbox:exec('/')
-]]
+function fileBrowser()
+	fbox= neutrino.CFileBrowser('/')
+	fbox:exec('/')
+end
 
 -- headers
---[[
-head = neutrino.CHeaders()
-head:paintHead(150,10,550,35,neutrino.NEUTRINO_ICON_MP3,"test", true,0,null,false)
-head:paintFoot(150, 570,550,35,550)
-]]
+function headers()
+	head = neutrino.CHeaders()
+	head:paintHead(150,10,550,35,neutrino.NEUTRINO_ICON_MP3,"test", true,0,null,false)
+	head:paintFoot(150, 570,550,35,550)
+end
 
 -- window
---[[
-w = neutrino.CWindow(150,45,550,525)
-w:paint()
-]]
+function window()
+	w = neutrino.CWindow(150,45,550,525)
+	w:paint()
+end
 
 -- scrollbar
---[[
-sb = neutrino.CScrollBar()
-sb:paint(150,45,525,10,1)
-]]
+function scrollBar()
+	sb = neutrino.CScrollBar()
+	sb:paint(150,45,525,10,1)
+end
 
 -- itemsdetailsline
---[[
-il = neutrino.CItems2DetailsLine()
-il:paint(150,10,550,615,80,35,30,1)
-]]
+function itemsDetailsline()
+	il = neutrino.CItems2DetailsLine()
+	il:paint(150,10,550,615,80,35,30,1)
+end
 
 -- hintbox
---[[
-hint = neutrino.CHintBox("neutrino: lua","first test\ntesting CHintBox\ndas ist alles ;-)")
-hint:exec()
-]]
+function hintBox()
+	hint = neutrino.CHintBox("neutrino: lua","first test\ntesting CHintBox\ndas ist alles ;-)")
+	hint:exec()
+end
 
 -- infobox
---[[
-info = neutrino.CInfoBox("first test\ntesting CHintBox\ndas ist alles ;-)")
-info:setText("first test\ntesting CHintBox\ndas ist alles ;-)")
-info:exec()
-]]
+function infoBox()
+	info = neutrino.CInfoBox("first test\ntesting CHintBox\ndas ist alles ;-)")
+	info:setText("first test\ntesting CHintBox\ndas ist alles ;-)")
+	info:exec()
+end
 
 -- framebuffer
---[[
-frameBuffer = neutrino.CFrameBuffer()
-frameBuffer:getInstance():paintBackground()
-]]
+function fb()
+	frameBuffer = neutrino.CFrameBuffer()
+	frameBuffer:getInstance():paintBackground()
+end
 
 -- listbox
---[[
-listbox = neutrino.ClistBox("test", neutrino.NEUTRINO_ICON_MOVIE)
-listbox:enablePaintDate()
-listbox:addWidget(neutrino.WIDGET_CLASSIC)
-listbox:addWidget(neutrino.WIDGET_FRAME)
-listbox:enableWidgetChange()
-listbox:exec(null, "")
-listbox:hide()
-]]
+function listBox()
+	listbox = neutrino.ClistBox("test", neutrino.NEUTRINO_ICON_MOVIE)
+	listbox:enablePaintDate()
+	listbox:addWidget(neutrino.WIDGET_CLASSIC)
+	listbox:addWidget(neutrino.WIDGET_FRAME)
+	listbox:enableWidgetChange()
+	listbox:exec(null, "")
+	listbox:hide()
+end
 
 -- menuwidget
---[[
-menu = neutrino.CMenuWidget("test", neutrino.NEUTRINO_ICON_MOVIE)
-menu:enableWidgetChange()
-menu:exec(null, "")
-menu:hide()
-]]
+function menuWidget()
+	menu = neutrino.CMenuWidget("test", neutrino.NEUTRINO_ICON_MOVIE)
+	menu:enableWidgetChange()
+
+	item1 = neutrino.CMenuForwarder("item1", true, "python-test", nil, "red_action")
+	item1:setIconName(neutrino.NEUTRINO_ICON_BUTTON_RED)
+	item1:setItemIcon(neutrino.NEUTRINO_ICON_BUTTON_RED)
+
+	item2 = neutrino.CMenuForwarder("item2", true, "python-test", nil, "green_action")
+	item3 = neutrino.CMenuForwarder("item3", true, "python-test", nil, "yellow_action")
+	item4 = neutrino.CMenuForwarder("item4", true, "python-test", nil, "blue_action")
+
+	menu:addItem(item1)
+	menu:addItem(item2)
+	menu:addItem(item3)
+	menu:addItem(item4)
+
+	menu:exec(null, "")
+	menu:hide()
+end
 
 -- listboxEntry
---[[
-listboxEntry = neutrino.ClistBoxEntry(150,10,550,615)
-listboxEntry:setTitle("test", neutrino.NEUTRINO_ICON_MOVIE)
-listboxEntry:enablePaintHead()
-listboxEntry:enablePaintDate()
-listboxEntry:enablePaintFoot()
-listboxEntry:paint()
-]]
+function listEntry()
+	listboxEntry = neutrino.ClistBoxEntry(150,10,550,615)
+	listboxEntry:setTitle("test", neutrino.NEUTRINO_ICON_MOVIE)
+	listboxEntry:enablePaintHead()
+	listboxEntry:enablePaintDate()
+	listboxEntry:enablePaintFoot()
+	listboxEntry:paint()
+end
 
 -- playback
---[[
-playback = neutrino.cPlayback()
-playback:Close()
-playback:Open()
-playback:Start("/home/mohousch/Music/AUD-20160209-WA0000.mp3")
-playback:Play()
-]]
+function player()
+	playback = neutrino.cPlayback()
+	playback:Close()
+	playback:Open()
+	playback:Start("/home/mohousch/Music/AUD-20160209-WA0000.mp3")
+	playback:Play()
+end
 
 -- audioplayergui
---[[
-aplay = neutrino.CAudioPlayerGui()
-aplay:addToPlaylist('/home/mohousch/Music/AUD-20160209-WA0000.mp3')
-aplay:exec(null,"")
-]]
+function audioPlayerGui()
+	aplay = neutrino.CAudioPlayerGui()
+	aplay:addToPlaylist('/home/mohousch/Music/AUD-20160209-WA0000.mp3')
+	aplay:exec(null,"")
+end
 
 -- movieplayergui
---[[
-mplay=neutrino.CMoviePlayerGui()
-mplay:addToPlaylist('/home/mohousch/Videos/ProSieben_20150619_201430.ts')
-mplay:exec(null,"")
-]]
+function moviePlayerGui()
+	mplay=neutrino.CMoviePlayerGui()
+	mplay:addToPlaylist('/home/mohousch/Videos/ProSieben_20150619_201430.ts')
+	mplay:exec(null,"")
+end
 
 -- pictureviewergui
---[[
-pic=neutrino.CPictureViewerGui()
-pic:addToPlaylist("/home/mohousch/Pictures/funart.png")
-pic:exec(null,"")
-]]
+function pictureViewerGui()
+	pic=neutrino.CPictureViewerGui()
+	pic:addToPlaylist("/home/mohousch/Pictures/funart.png")
+	pic:exec(null,"")
+end
 
 -- CPlugins
---[[
-plugins = neutrino.CPlugins()
-plugins:startPlugin("youtube")
-]]
+function startPlugin()
+	plugins = neutrino.CPlugins()
+	plugins:startPlugin("youtube")
+end
 
+-- main
+function main()
+	messageBox()
+end
 
+main()
 
 
 
--- plugins/python/python.py
+++ plugins/python/python.py
@@ -1,90 +1,128 @@
 print("python: Hello World")
 
-from neutrino import CMessageBox, CHelpBox, CHeaders, CWindow, CScrollBar, CItems2DetailsLine, CPictureViewer, CFrameBuffer, ClistBox, CMenuWidget, ClistBoxEntry, CInfoBox, cPlayback, CAudioPlayerGui, CMoviePlayerGui, CPictureViewerGui, CPlugins
+from neutrino import CMessageBox, CHelpBox, CHeaders, CWindow, CScrollBar, CItems2DetailsLine, CPictureViewer, CFrameBuffer, ClistBox, CMenuWidget, CMenuForwarder, ClistBoxEntry, CInfoBox, cPlayback, CAudioPlayerGui, CMoviePlayerGui, CPictureViewerGui, CPlugins, CFile
 
-msg = CMessageBox("neutrino: python", "first test\ntesting CMessageBox\ndas ist alles ;-)")
-msg._exec(-1)
+## CMessageBox
+def messageBox():
+	msg = CMessageBox("neutrino: python", "first test\ntesting CMessageBox\ndas ist alles ;-)")
+	msg._exec(-1)
 
-## helpbox
-#hbox = CHelpBox()
-#hbox.addLine("neutrino: python")
-#hbox.addSeparator()
-#hbox.addLine("first test")
-#hbox.addPageBreak()
-#hbox.addLine("testing CHelpBox\ndas ist alles ;-)")
-#hbox.show("CHelpBox: python")
+## CHelpBox
+def helpBox():
+	hbox = CHelpBox()
+	hbox.addLine("neutrino: python")
+	hbox.addSeparator()
+	hbox.addLine("first test")
+	hbox.addPageBreak()
+	hbox.addLine("testing CHelpBox\ndas ist alles ;-)")
+	hbox.show("CHelpBox: python")
 
-## headers
-#CHeaders().paintHead(150,10,550,35,"mp3","test", 1,0,None,0)
-#CHeaders().paintFoot(150, 570,550,35,550)
+## CHeaders
+def headers():
+	CHeaders().paintHead(150,10,550,35,"mp3","test", 1,0,None,0)
+	CHeaders().paintFoot(150, 570,550,35,550)
 
-## window
-#CWindow(150,45,550,525).paint()
+## CWindow
+def window():
+	CWindow(150,45,550,525).paint()
 
-## scrollbar
-#CScrollBar().paint(150,45,525,10,1)
+## CScrollBar
+def scrollBar():
+	CScrollBar().paint(150,45,525,10,1)
 
-## itemsdetailsline
-#CItems2DetailsLine().paint(150,10,550,615,80,35,30,1)
+## CItemsdetailsline
+def items2Detailsline():
+	CItems2DetailsLine().paint(150,10,550,615,80,35,30,1)
 
-## infobox
-#infoBox = CInfoBox("first test\ntesting CHintBox\ndas ist alles ;-)")
-#infoBox.setText("first test\ntesting CHintBox\ndas ist alles ;-)")
-#infoBox._exec()
+## CInfoBox
+def infoBox():
+	infoBox = CInfoBox("first test\ntesting CHintBox\ndas ist alles ;-)")
+	infoBox.setText("first test\ntesting CHintBox\ndas ist alles ;-)")
+	infoBox._exec()
 
-## framebuffer
-#frameBuffer=CFrameBuffer()
-#CFrameBuffer.paintBackground()
+## CFrameBuffer
+def fb():
+	frameBuffer=CFrameBuffer()
+	CFrameBuffer.paintBackground()
 
-## listbox
-#listbox = ClistBox("test", "mp3")
-#listbox.enablePaintDate()
-##listbox.addWidget(WIDGET_CLASSIC)
-##listbox.addWidget(WIDGET_FRAME)
-##listbox.enableWidgetChange()
-#listbox._exec(None, "")
-#listbox.hide()
+## ClistBox
+def listBox():
+	listbox = ClistBox("test", "mp3")
+	listbox.enablePaintDate()
+	listbox.addWidget(WIDGET_CLASSIC)
+	listbox.addWidget(WIDGET_FRAME)
+	listbox.enableWidgetChange()
+	listbox._exec(None, "")
+	listbox.hide()
 
-## menuwidget
-#menu = CMenuWidget("test", "mp3")
-#menu.enableWidgetChange()
-#menu.enableFootInfo()
-#menu._exec(None, "")
-#menu.hide()
+## CMenuWidget
+def menuWidget():
+	menu = CMenuWidget("test", "mp3")
+	menu.enableWidgetChange()
+	menu.enableFootInfo()
 
-## listboxEntry
-#listboxEntry = ClistBoxEntry(150,10,550,615)
-#listboxEntry.setTitle("test", "mp3")
-#listboxEntry.enablePaintHead()
-#listboxEntry.enablePaintDate()
-#listboxEntry.enablePaintFoot()
-#listboxEntry.paint()
+	item1 = CMenuForwarder("item1", True, "python-test", None, "red_action")
+	item2 = CMenuForwarder("item2", True, "python-test", None, "green_action")
+	item3 = CMenuForwarder("item3", True, "python-test", None, "yellow_action")
+	item4 = CMenuForwarder("item4", True, "python-test", None, "blue_action")
 
-## playback
-#playback = cPlayback(1)
-#playback.Close()
-#playback.Open()
-#playback.Start("/home/mohousch/Music/AUD-20160209-WA0000.mp3")
-#playback.Play()
+	menu.addItem(item1)
+	menu.addItem(item2)
+	menu.addItem(item3)
+	menu.addItem(item4)
 
-## audioplayergui
-#aplay = CAudioPlayerGui()
-#aplay.addToPlaylist('/home/mohousch/Music/AUD-20160209-WA0000.mp3')
-#aplay._exec(None,"")
+	menu._exec(None, "")
+	menu.hide()
 
-## movieplayergui
-#mplay=CMoviePlayerGui()
-#mplay.addToPlaylist('/home/mohousch/Videos/ProSieben_20150619_201430.ts')
-#mplay._exec(None,"")
+## ClistBoxEntry
+def listEntry():
+	listboxEntry = ClistBoxEntry(150,10,550,615)
+	listboxEntry.setTitle("test", "mp3")
+	listboxEntry.enablePaintHead()
+	listboxEntry.enablePaintDate()
+	listboxEntry.enablePaintFoot()
+	listboxEntry.paint()
 
-## pictureviewergui
-#pic=CPictureViewerGui()
-#pic.addToPlaylist("/home/mohousch/Pictures/funart.png")
-#pic._exec(None,"")
+## cPlayback
+def playBack():
+	playback = cPlayback(1)
+	playback.Close()
+	playback.Open()
+	playback.Start("/home/mohousch/Music/AUD-20160209-WA0000.mp3")
+	playback.Play()
+
+## CAudioPlayerGui
+def audioPlayerGui():
+	aplay = CAudioPlayerGui()
+	aplay.addToPlaylist('/home/mohousch/Music/AUD-20160209-WA0000.mp3')
+	aplay._exec(None,"")
+
+## CMoviePlayerGui
+def moviePlayerGui():
+	mplay=CMoviePlayerGui()
+	mplay.addToPlaylist('/home/mohousch/Videos/ProSieben_20150619_201430.ts')
+	mplay._exec(None,"")
+
+## CPictureViewerGui
+def pictureViewerGui():
+	pic=CPictureViewerGui()
+	pic.addToPlaylist("/home/mohousch/Pictures/funart.png")
+	pic._exec(None,"")
 
 ## CPlugins
-#plugins = CPlugins()
-#plugins.startPlugin("nfilm")
+def plugins():
+	plugins = CPlugins()
+	plugins.startPlugin("nfilm")
+
+
+class main():
+	def __init__(self, session):
+		self.session = session
+
+	messageBox()
+
+def start(session, **kwargs):
+	session.open(main)
 
 
 
